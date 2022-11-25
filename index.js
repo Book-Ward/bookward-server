e5be4d0059892e2198028ed8945e74cd5953f68a
Iskren Alexandrov <iskren.alexandrov@gmail.com>
2022-11-25 22:38:40 +0200

    Add MongoDB connection and test endpoint

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const express = require('express');
-  
+const express = require("express");
+const mongoose = require("mongoose");
+const Book = require("./models/Book");
+require("dotenv").config();
+
 const app = express();
-const PORT = 3000;
-  
-app.listen(PORT, (error) =>{
-    console.log("Hello");
-});
+
+app.get('/books', async (req, res)=>{
+    try{
+        const data = await Book.find().limit(10);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }});
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ models/Book.js
@@ -0,0 +1,77 @@
+const { model, Schema } = require("mongoose");
+
+const bookSchema = new Schema({
+  bookId: String,
+  title: String,
+  author: String,
+  rating: Number,
+  description: {
+    type: String,
+    default: "",
+  },
+  language: String,
+  isbn: Number,
+  genres: Array,
+  characters: {
+    type: Array,
+    default: [],
+  },
+  bookFormat: {
+    type: String,
+    default: "",
+  },
+  edition: {
+    type: String,
+    default: "",
+  },
+  pages: {
+    type: Number,
+    default: 0,
+  },
+  publisher: {
+    type: String,
+    default: "",
+  },
+  publishDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  awards: {
+    type: Array,
+    default: [],
+  },
+  numRatings: {
+    type: Number,
+    default: 0,
+  },
+  ratingsByStars: {
+    type: Array,
+    default: [],
+  },
+  likedPercent: {
+    type: Number,
+    default: 0,
+  },
+  setting: {
+    type: Array,
+    default: [],
+  },
+  coverImg: {
+    type: String,
+    default: "",
+  },
+  bbeScore: {
+    type: Number,
+    default: 0,
+  },
+  bbeVotes: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = model("Book", bookSchema);
